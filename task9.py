2ab8133b99354543a73516e71382c049f2de11a1
honest <honest.htc@gmail.com>
2021-06-15 00:04:10 +0300

    done  9 task

--- task9.py
+++ task9.py
@@ -2,34 +2,33 @@
 
 k_num = 0
 s_num = 0
-n_max = 0
-n_min = 0
 s_arif_num = 0
-n = 1
+k_chet = 0
+k_nchet = 0
+n = int(input('input value: '))
+n_max = n
+n_min = n
 while n != 0:
+    if n == 0:
+        continue
     if n > n_max:
         n_max = n
-    if n < n_max:
+    if n < n_min:
         n_min = n
-    n = int(input('input value: '))
+    if n % 2 == 0:
+        k_chet += 1
+    else:
+        k_nchet += 1
     k_num += 1
     s_num += n
-
-
+    n = int(input('input value: '))
 else:
     s_arif_num = s_num / k_num
-    print('количесво введенных', k_num)
-    print('Сумма', s_num)
-    print('Средне арифмет', s_arif_num)
-    print('Максимальное', n_max)
-    print('Минимальное', n_min)
-
-# количество введённых чисел - это i
-# их сумму это q
-# среднее арифметическое всех введённых чисел = общую сумму поделить на кол-во введенных попыток
-# максимальное через иф наверно большее или меньшее
-# минимальное значение
-# количество чётных
-# не чётных значений - в цикл загнать переменную которая будет проверять четные и не четные и отдельно их счтитаь
-# print(n, '   ', q, ' ', i, '   2 ** ', q, ' = ', i, sep='')
-
+    print()
+    print('Количесво введенных чисел', k_num)
+    print('Сумма введенных чисел', s_num)
+    print('Средне арифметическое', s_arif_num)
+    print('Максимальное число', n_max)
+    print('Минимальное число', n_min)
+    print('Количесво четных', k_chet)
+    print('Количесво нечетных', k_nchet)
