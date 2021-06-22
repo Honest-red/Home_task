d3d78a0a100833b296777fec8b04bb65b6ec597f
honest_small_pc <honest.htc@gmail.com>
2021-06-22 16:07:30 +0300

    done task14

---
+++ task14_test.py
@@ -0,0 +1,18 @@
+#ДЗ 14. Треугольник 1
+
+rows = int(input('Input a high: '))
+cols = (rows*2)-1
+
+for i in range(rows):
+    print(i, end='\t')
+    for j in range(cols):
+        if (
+            i == rows-1
+            or j == rows-i-1
+            or j == rows+i-1
+        ):
+            print('*  ', end='')
+        else:
+            print('   ', end='')
+    print()
+print()
