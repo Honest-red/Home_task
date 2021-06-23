2351a0691173d42574b2ee7805aafc5abfa108db
honest_small_pc <honest.htc@gmail.com>
2021-06-23 11:03:02 +0300

    done task16

--- task17.py
+++ task17.py
@@ -1,4 +1,4 @@
-#ДЗ 14. Ромб 1
+#ДЗ 14. Ромб 2
 
 rows = int(input('Input a high: '))
 cols = rows
@@ -6,11 +6,13 @@ for i in range(rows):
     print(i, end='\t')
     for j in range(cols):
         if (
-            j == (rows//2)-i
+            i == (rows-1)//2
+            or j == (rows//2)-i
             or j == (rows//2)+i
             or i == (cols//2)+j
             or i == (cols//2)+(cols-1-j)
-            or (j > (rows//2)-i and j < (rows//2)+i and i <= (rows-1)//2)
+            or (j > (rows//2)-i and j < (rows//2)+i and i < (rows-1)//2)
+            or
         ):
             print('*  ', end='')
         else:
