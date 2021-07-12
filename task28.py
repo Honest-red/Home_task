f392f47ea9c098004cd428ab697b35d51728b710
honest <honest.htc@gmail.com>
2021-07-13 00:55:53 +0300

    work in task 28

---
+++ task28.py
@@ -0,0 +1,19 @@
+from random import randint
+
+
+#m = int(input('Input value: '))
+m = 10
+
+
+
+#for _ in range(m):
+lst = [[randint(1, 50) for _ in range(m)] for _ in range(m)]
+
+
+  #  lst = [randint(1, 50) for _ in range(m)]
+
+#print(lst, end='')
+
+
+#sum_num = sum([int(m) for m in lst])
+#print(sum_num)
---
+++ task28_test.py
@@ -0,0 +1,38 @@
+from random import randint
+
+m = 5
+lst2 = []
+
+lst = [[randint(1, 50) for i in range(m)] for j in range(m)]
+
+# lst = [randint(1, 50) for i in range(m)]
+print(lst)
+
+print()
+
+
+def v_print(lsk):
+    for i in range(m):
+        for j in range(m):
+
+            if lst[i][j] < 0b1010:
+                lst2.append(' ' + str(lst[i][j]))
+            else:
+                lst2.append(lst[i][j])
+
+            print(lst2[i], end=' ')
+        print(lst2[j])
+
+    # print(' ', end='\n')
+    # lst2.append(lst[i][j])
+    # print(lst2)
+    # print(lst2[i], end='\n')
+    # print('III', i)
+    # print('jjjj', j)
+
+    # print(lst2[j], end='\n')
+
+
+print(v_print(lst))
+
+print(lst2)
