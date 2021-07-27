be684392463d07d3c15f492a439adacc28581b5a
Honest-red <85483359+Honest-red@users.noreply.github.com>
2021-07-27 10:37:20 +0300

    Update task31.py
    done_task31

--- task31.py
+++ task31.py
@@ -1,39 +1,15 @@
-# В работе!!1
+# ДЗ 31. Реверс списка
 
-from random import randint
-
-def qsort(nums, first_idx, last_idx):
-    if first_idx >= (first_idx + last_idx) // 2:
+def sort(lst1, first_idx, last_idx):
+    if first_idx >= len(lst1) // 2:
         return
-    
-    print(last_idx)
-    #print((first_idx + last_idx) // 2)
-    #print()
-    k = 0
-
-    i, j = first_idx, last_idx
+    lst1[first_idx], lst1[last_idx] = lst1[last_idx], lst1[first_idx]
+    sort(lst1, first_idx + 1, last_idx - 1)
 
 
-    #middle_value = nums[(first_idx + last_idx) // 2]
-    while i <= (first_idx + last_idx) // 2:
-        #while nums[i] < middle_value:
-            #i += 1
-        #while nums[j] > middle_value:
-            #j -= 1
-        #if i <= j:
-        nums[i], nums[j] = nums[j], nums[i]
-        i += 1
-        j -= 1
-        k += 1
-        print(k)
-    qsort(nums, first_idx, j)
-    qsort(nums, i, last_idx)
-
-st = 'file Program Files Python file python Python lessons Git Home task task python file Task python mast' \
-     ' fle task 21 is correct and lessons Python dict is done. Python is good file '
+st = 'file Program Files Python file python Python lessons Git Home task task python file Task python mast'
 lst = st.split()
 
 print(lst)
-qsort(lst, 0, len(lst)-1)
+sort(lst, 0, len(lst)-1)
 print(lst)
-print(len(lst))
