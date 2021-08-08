e3e2621157778675f19ad8b40efb654dea166e48
Honest-red <85483359+Honest-red@users.noreply.github.com>
2021-08-08 10:26:38 +0300

    Create lessons16.py

---
+++ lessons16.py
@@ -0,0 +1,15 @@
+# * args - список именованных аргументов,  кортеж получим
+# ** kwargs -  список не именованых, словарь получим
+#
+# def __init__(self, v1=None, v2=None, v3=None, v4=None):
+#     if v1 is None: (проверка)
+#      #self.v = list - vtoroi variant
+#         self.v1 = Point()
+#         #self.v.append(Point()) - vtori variant
+#     else:
+#         self.v1 = v1
+#     self.v2 = v2
+#     self.v3 = v3
+#     self.v4 = v4
+#
+
